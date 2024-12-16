0de5f7aba7d222fcbbf77d33d96f196742eac750
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2024-12-16 21:24:33 +0200

    Implement LoginCubit and associated states for user authentication

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:chat_app/pages/login_page.dart';
 import 'package:chat_app/pages/register_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'firebase_options.dart';
 
 void main() async {
---
+++ lib/pages/cubits/login_cubit/login_state.dart
@@ -0,0 +1,12 @@
+part of 'login_cubit.dart';
+
+@immutable
+abstract class LoginState {}
+
+class LoginInitial extends LoginState {}
+
+class LoginScuess extends LoginState {}
+
+class LoginLoading extends LoginState {}
+
+class LoginFailure extends LoginState {}
